5edc6cc3c71f8ee4f6ac2f43cbdf057892d9c553
Andrii Mishchenko <vinnypuh1818@gmail.com>
2023-11-03 11:35:44 +0200

    library full resp

--- src/components/Library/Library.js
+++ src/components/Library/Library.js
@@ -2,6 +2,9 @@ import React from 'react';
 import '../Library/Library.css';
 import firstPost from '../../assets/Rectangle 22-4.png'
 import secondPost from '../../assets/Rectangle 22.png'
+import thirdPost from '../../assets/Rectangle 22-1.png'
+import fourthPost from '../../assets/Rectangle 22-2.png'
+import fifthPost from '../../assets/Rectangle 22-3.png'
 
 const Library = () => {
   return (
@@ -10,27 +13,51 @@ const Library = () => {
         <p>A lot is happening, We are blogging about it.</p>
       </div>
       <div className='library-block-library'>
-        <div className='library-block-library-firstPost'>
+        <div className='library-block-library-leftBlock'>
           <img src={firstPost}></img>
-          <div className='library-block-library-firstPost-description'>
-            <p className='library-block-library-firstPost-description-date'>Sep 26, 2021</p>
+          <div className='library-block-library-leftBlock-description'>
+            <p className='library-block-library-leftBlock-description-data'>Sep 26, 2021</p>
             <h3>GPT-3 and Open  AI is the future. Let us exlore how it is?</h3>
-            <p className='library-block-library-firstPost-description-date-link'>Read Full Article</p>
+            <p className='library-block-library-leftBlock-description-link'>Read Full Article</p>
           </div>
         </div>
-        
-        <div className='library-block-library-secondBlockPost'>
-          <div className='library-block-library-secondBlockPost-secondPost'>
-            <img src={secondPost}></img>
-            <div className='library-block-library-secondBlockPost-description'>
-              <p className='library-block-library-secondBlockPost-description-date'>Sep 26, 2021</p>
-              <h3>GPT-3 and Open  AI is the future. Let us exlore how it is?</h3>
-              <p className='library-block-library-secondBlockPost-description-date-link'>Read Full Article</p>
+        <div className='library-block-library-rightBlock'>
+          <div className='library-block-library-rightBlock-firstLine'>
+            <div className='library-block-library-rightBlock-firstLine-leftBox'>
+              <img src={secondPost}></img>
+              <div className='library-block-library-rightBlock-firstLine-leftBox-description'>
+                <p className='library-block-library-rightBlock-firstLine-leftBox-description-data'>Sep 26, 2021</p>
+                <h3>GPT-3 and Open  AI is the future. Let us exlore how it is?</h3>
+                <p className='library-block-library-rightBlock-firstLine-leftBlock-description-link'>Read Full Article</p>
+              </div>
+            </div>
+            <div className='library-block-library-rightBlock-firstLine-rightBox'>
+              <img src={thirdPost}></img>
+              <div className='library-block-library-rightBlock-firstLine-rightBox-description'>
+                <p className='library-block-library-rightBlock-firstLine-rightBox-description-data'>Sep 26, 2021</p>
+                <h3>GPT-3 and Open  AI is the future. Let us exlore how it is?</h3>
+                <p className='library-block-library-rightBlock-firstLine-rightBox-description-link'>Read Full Article</p>
+              </div>
             </div>
           </div>
-          <div className='library-block-library-secondBlockPost-thirdPost'></div>
-          <div className='library-block-library-secondBlockPost-fourthPost'></div>
-          <div className='library-block-library-secondBlockPost-fifthPost'></div>
+          <div className='library-block-library-rightBlock-secondLine'>
+            <div className='library-block-library-rightBlock-secondLine-leftBox'>
+              <img src={fourthPost}></img>
+              <div className='library-block-library-rightBlock-secondLine-leftBox-description'>
+                <p className='library-block-library-rightBlock-secondLine-leftBox-description-data'>Sep 26, 2021</p>
+                <h3>GPT-3 and Open  AI is the future. Let us exlore how it is?</h3>
+                <p className='library-block-library-rightBlock-secondLine-leftBlock-description-link'>Read Full Article</p>
+              </div>
+            </div>
+            <div className='library-block-library-rightBlock-secondLine-rightBox'>
+              <img src={fifthPost}></img>
+              <div className='library-block-library-rightBlock-secondLine-rightBox-description'>
+                <p className='library-block-library-rightBlock-secondLine-rightBox-description-data'>Sep 26, 2021</p>
+                <h3>GPT-3 and Open  AI is the future. Let us exlore how it is?</h3>
+                <p className='library-block-library-rightBlock-firstLine-rightBox-description-link'>Read Full Article</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
